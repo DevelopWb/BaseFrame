32b6e992faf4a63a2c4906492d6d6bbf8c35fb62
tobato <bin_w_ing@foxmail.com>
2022-06-09 21:27:46 +0800

    junze

--- app/src/main/java/com/juntai/wisdom/project/utils/bannerImageLoader/GlideImageLoader.java
+++ app/src/main/java/com/juntai/wisdom/project/utils/bannerImageLoader/GlideImageLoader.java
@@ -6,16 +6,10 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.juntai.disabled.basecomponent.utils.ImageLoadUtil;
-import com.shuyu.gsyvideoplayer.GSYVideoManager;
-import com.shuyu.gsyvideoplayer.player.PlayerFactory;
-import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
 import com.youth.banner.loader.ImageLoaderInterface;
-
-import tv.danmaku.ijk.media.exo2.Exo2PlayerManager;
 
 
 public class GlideImageLoader implements ImageLoaderInterface<View> {
-    StandardGSYVideoPlayer videoPlayer;
     long seek = 0;
     String  pathV;
     private OnFullScreenListener onFullScreenListener;
@@ -45,7 +39,6 @@ public class GlideImageLoader implements ImageLoaderInterface<View> {
             ImageLoadUtil.loadImageCache(context, (String) bannerObject.getEventObj(),imageView);
         } else if (BannerObject.BANNER_TYPE_VIDEO.equals(eventKey)) {
             pathV = (String) bannerObject.getEventObj();
-            startVideo(context);
         } else if (BannerObject.BANNER_TYPE_RTMP.equals(eventKey)) {
 //            ImageView imageView = (ImageView) view;
 //
@@ -66,8 +59,7 @@ public class GlideImageLoader implements ImageLoaderInterface<View> {
             ImageView simpleDraweeView = new ImageView(context);
             return simpleDraweeView;
         } else if (BannerObject.BANNER_TYPE_VIDEO.equals(eventKey)) {
-            videoPlayer = new StandardGSYVideoPlayer(context);
-            return videoPlayer;
+            return null;
         } else if (BannerObject.BANNER_TYPE_RTMP.equals(eventKey)) {
             // 流的封面图
             ImageView rtmpView = new ImageView(context);
@@ -78,46 +70,7 @@ public class GlideImageLoader implements ImageLoaderInterface<View> {
 
     }
 
-    private ImageView startVideo(Context mContext) {
-        //RTMP播放需切换至exo播放
-        PlayerFactory.setPlayManager(Exo2PlayerManager.class);
-        videoPlayer.setUp(pathV, false, "");
-        //增加封面
-        ImageView imageView = new ImageView(mContext);
-        Glide.with(mContext).asBitmap().load(pathV).into(imageView);
-        videoPlayer.setThumbImageView(imageView);
-        //增加title
-        videoPlayer.getTitleTextView().setVisibility(View.GONE);
-        //设置返回键
-        videoPlayer.getBackButton().setVisibility(View.GONE);
-        videoPlayer.seekTo(seek);
-        //全屏键
-        videoPlayer.getFullscreenButton().setVisibility(View.VISIBLE);
-        videoPlayer.getFullscreenButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (onFullScreenListener != null) {
-                    onFullScreenListener.onFullScreen();
-                }
-            }
-        });
-        //是否可以滑动调整
-        videoPlayer.setIsTouchWiget(false);
-        //设置返回按键功能
-        videoPlayer.getBackButton().setVisibility(View.GONE);
-        videoPlayer.getLayoutId();
-        return imageView;
-    }
 
-    public void pause(){
-        if (videoPlayer != null){
-            GSYVideoManager.onPause();
-        }
-    }
-    //释放所有
-    public void release(){
-        GSYVideoManager.releaseAllVideos();
-    }
 
     /**
      * 全屏得点击事件
