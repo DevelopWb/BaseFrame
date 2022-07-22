b4e2f7b9b6364c14d6f59065b693e1e1d4504306
tobato <188827872@qq.com>
2022-07-22 14:43:30 +0800

    解析适配器数据

--- app/src/main/java/a3phone/of/com/main/multi/BaseMultiActivity.java
+++ app/src/main/java/a3phone/of/com/main/multi/BaseMultiActivity.java
@@ -2,15 +2,18 @@ package a3phone.of.com.main.multi;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.ArrayMap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.entity.MultiItemEntity;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import a3phone.of.com.main.R;
 import a3phone.of.com.main.base.BaseRecyclerviewActivity;
@@ -34,6 +37,8 @@ import okhttp3.MultipartBody;
  */
 public abstract class BaseMultiActivity extends BaseRecyclerviewActivity<MultiPresent> implements BaseIView, SelectPhotosFragment.OnPhotoItemClick, MultiContact.BaseNormalRvItemClick, View.OnClickListener {
     public TextView mCommitTv;
+    private TextKeyValueBean selectBean;
+    private TextView selectTv;
 
 
     @Override
@@ -61,8 +66,8 @@ public abstract class BaseMultiActivity extends BaseRecyclerviewActivity<MultiPr
         MultipleItem multipleItem = (MultipleItem) adapter.getItem(position);
         switch (multipleItem.getItemType()) {
             case MultipleItem.ITEM_SELECT:
-                TextKeyValueBean selectBean = (TextKeyValueBean) multipleItem.getObject();
-                TextView selectTv = (TextView) adapter.getViewByPosition(recyclerView, position, R.id.select_value_tv);
+                selectBean = (TextKeyValueBean) multipleItem.getObject();
+                selectTv = (TextView) adapter.getViewByPosition(recyclerView, position, R.id.select_value_tv);
                 String selectKey = selectBean.getKey();
                 switch (selectKey) {
                     case MultiContact.PURCHASE_TYPE:
@@ -73,8 +78,10 @@ public abstract class BaseMultiActivity extends BaseRecyclerviewActivity<MultiPr
                         PickerManager.getInstance().showTimePickerView(mContext, new boolean[]{true, true, true, false, false, false}, MultiContact.PURCHASE_DELIVER_TIME, new PickerManager.OnTimePickerTimeSelectedListener() {
                             @Override
                             public void onTimeSelect(Date date, View v) {
-                                selectTv.setText(CalendarUtil.getTimeFromDate("yyyy-MM-dd",
-                                        date));
+                                String time = CalendarUtil.getTimeFromDate("yyyy-MM-dd",
+                                        date);
+                                selectTv.setText(time);
+                                selectBean.setValue(time);
                             }
                         });
                         break;
@@ -106,8 +113,22 @@ public abstract class BaseMultiActivity extends BaseRecyclerviewActivity<MultiPr
      *
      * @return
      */
-    protected BaseAdapterDataBean getBaseAdapterData(List<MultipleItem> arrays) {
+    protected BaseAdapterDataBean getBaseAdapterData() {
+        Map<String, Object> map = new ArrayMap<>();
         BaseAdapterDataBean bean = new BaseAdapterDataBean();
+        map = resolveData(baseQuickAdapter.getData(), map);
+        map.size();
+        return bean;
+
+    }
+
+    /**
+     * 解析适配器数据
+     * @param arrays
+     * @param map
+     * @return
+     */
+    private Map<String, Object> resolveData(List<MultipleItem> arrays, Map<String, Object> map) {
         for (MultipleItem array : arrays) {
             switch (array.getItemType()) {
                 case MultipleItem.ITEM_NORMAL_RECYCLEVIEW:
@@ -116,7 +137,7 @@ public abstract class BaseMultiActivity extends BaseRecyclerviewActivity<MultiPr
                     switch (baseNormalRecyclerviewBean.getType()) {
                         case MultiContact.BASE_RECYCLERVIEW_TYPE_MULTI:
                             List<MultipleItem> multipleItems = (List<MultipleItem>) baseNormalRecyclerviewBean.getObject();
-                            BaseAdapterDataBean baseAdapterDataBean = getBaseAdapterData(multipleItems);
+                            resolveData(multipleItems, map);
 
                             break;
                         default:
@@ -127,13 +148,13 @@ public abstract class BaseMultiActivity extends BaseRecyclerviewActivity<MultiPr
 
                 case MultipleItem.ITEM_SELECT:
                     TextKeyValueBean selectBean = (TextKeyValueBean) array.getObject();
+                    String selectValue = selectBean.getValue();
                     switch (selectBean.getKey()) {
                         case MultiContact.PURCHASE_TYPE:
-                            ToastUtils.toast(mContext, "采购类型");
+                           map.put(MultiContact.PURCHASE_TYPE,selectValue);
                             break;
                         case MultiContact.PURCHASE_DELIVER_TIME:
-                            ToastUtils.toast(mContext, "时间");
-
+                            map.put(MultiContact.PURCHASE_DELIVER_TIME,selectValue);
                             break;
                         default:
                             break;
@@ -141,10 +162,14 @@ public abstract class BaseMultiActivity extends BaseRecyclerviewActivity<MultiPr
                     break;
                 case MultipleItem.ITEM_EDIT_VER:
                     TextKeyValueBean editVerBean = (TextKeyValueBean) array.getObject();
+                    String editVerValue = editVerBean.getValue();
+
                     switch (editVerBean.getKey()) {
                         case MultiContact.PURCHASE_REASON:
+                            map.put(MultiContact.PURCHASE_REASON,editVerValue);
                             break;
                         case MultiContact.REMARK:
+                            map.put(MultiContact.REMARK,editVerValue);
                             break;
                         default:
                             break;
@@ -154,6 +179,13 @@ public abstract class BaseMultiActivity extends BaseRecyclerviewActivity<MultiPr
                     ItemFragmentBean fragmentBean = (ItemFragmentBean) array.getObject();
                     switch (fragmentBean.getTitle()) {
                         case MultiContact.REMARK_PICS:
+                            List<String>  pics = fragmentBean.getFragmentPics();
+                            if (pics != null) {
+                                for (String pic : pics) {
+                                    map.put(MultiContact.REMARK_PICS,pic);
+                                }
+                            }
+
                             break;
                         default:
                             break;
@@ -163,8 +195,7 @@ public abstract class BaseMultiActivity extends BaseRecyclerviewActivity<MultiPr
                     break;
             }
         }
-        return bean;
-
+        return map;
     }
 
     @Override
@@ -215,7 +246,7 @@ public abstract class BaseMultiActivity extends BaseRecyclerviewActivity<MultiPr
         switch (v.getId()) {
             case R.id.commit_form_tv:
                 // TODO: 2022/7/22 提交
-                getBaseAdapterData(baseQuickAdapter.getData());
+                getBaseAdapterData();
                 break;
             default:
                 break;
